758a69eba7fdcfc2e25d8f504c9e35b2b0f39259
Florent <florent.carrez.fc@gmail.com>
2021-01-22 10:36:07 +0100

    Create Utility pkg and fulfill GameInfo

--- Snake/src/Context/gamecontexts-gameinfo.ads
+++ Snake/src/Context/gamecontexts-gameinfo.ads
@@ -1,5 +1,26 @@
 package GameContexts.GameInfo is
 
-   
+	type Level is ( Lv1, Lv2, LvCustom);
+	
+	type GameInfo is private;
+	
+	procedure SetUpGameInfo(ctxt: in out GameContext.Context);
+	
+	function Running(g: in GameInfo) return Boolean;
+	function Pausing(g: in GameInfo) return Boolean;
+	
+	procedure StopGame(g: in out GameInfo);
+	
+private
+	
+	type GameInfo is tagged record
+		running: Boolean := False;
+		pausing: Boolean := False;
+		level: Level := Lv1;
+		lvRef: String; -- TODO: find a way to store variadic length string
+	end record;
+	
+	procedure displayPlayPan;
+	
 
 end GameContexts.GameInfo;
--- Snake/src/utility.ads
+++ Snake/src/utility.ads
@@ -1,4 +1,21 @@
+with Interfaces.C; use Interfaces.C;
+with Interfaces.C.Strings;
 package Utility is
+
+	type Pos is record
+		x, y: Integer;
+	end record;
+
+
+
+
+	type ColorName is (Blue, Red, Green, Brown, Black);
+	function Color(c: ColorName) return int;
+
+	-- ADA API for the private C function linked, used to navigate in the term
+	procedure Print(str: String; from: Pos := (-1, -1));
+	procedure Move_to(x, y: Integer);
+	procedure SetColor(c: ColorName);
 
 
 end Utility;
