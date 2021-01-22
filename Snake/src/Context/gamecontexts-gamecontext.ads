4777b643f7b3d7db9bb1bc4e1384cfffecc6a5cd
Florent <florent.carrez.fc@gmail.com>
2021-01-22 11:36:22 +0100

    Spec of the Context

--- Snake/src/Context/gamecontexts-gamecontext.ads
+++ Snake/src/Context/gamecontexts-gamecontext.ads
@@ -2,9 +2,20 @@ package GameContexts.GameContext  is
 
 	type Context is private;
 	
+	function CreatContext(width, height: Positive) return Context;
+	
+	function MaxWidth(ctxt: in Context) return Positive;
+	function MaxHeight(ctxt: in Context) return Positive;
+	function Config(ctxt: in Context) return Config;
+	function Game(ctxt: in out Context) return access GameInfo; 
+	-- return *GameInfo, we want to be able to modify it
+	-- not: to return the access to the GameInfo, we must use: 'Access or 'Unchecked_Access
+	-- the in out allows us to return a non const access.
+	
 private
-	type Context is tagged record
-		I: Integer;
+	type Context(maxWidth, maxHeight: Positive) is tagged record
+		conf: Config;
+		game: aliased GameInfo; -- aliased means that it is store on the stack (and then has an address) instead of on a register
 	end record;
    
 
