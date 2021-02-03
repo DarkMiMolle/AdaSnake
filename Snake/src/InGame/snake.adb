a3ed2eea2aa708c6ec5623ab9646e78a704a919a
raphael.treglia <32885615+TregliaRaphael@users.noreply.github.com>
2021-02-03 11:48:51 +0100

    Update snake.adb

--- Snake/src/InGame/snake.adb
+++ Snake/src/InGame/snake.adb
@@ -1,44 +1,43 @@
 package body Snake is
 
-   function NextPosFrom(dir: in Direction.Dir; pos: Position) return Position is
-   begin
-   end NexPosFrom;
+   	function NextPosFrom(dir: in Direction.Dir; pos: Position) return Position is
+	begin
+   	end NexPosFrom;
 	
-	function Creat(ctxt: in out GameContext.Context) return Snake is
-   begin
-   end Creat;
+   	function Creat(ctxt: in out GameContext.Context) return Snake is
+   	begin
+   	end Creat;
 	
-	procedure Display(s: in Snake) is
-   begin
-   end Display;
+   	procedure Display(s: in Snake) is
+   	begin
+   	end Display;
    
 	procedure Hide(s: in Snake) is
-   begin
-   end Hide;
+   	begin
+   	end Hide;
 	
 	procedure Move(s: in out Snake) is
-   begin
-   end Move;
+   	begin
+   	end Move;
    
 	procedure Pos(s: in out Snake; p: Position) is
-   begin
-   end Pos;
+   	begin
+   	end Pos;
    
 	function Pos(s: in Snake) return Position is
-   begin
-   end Pos;
+   	begin
+   	end Pos;
    
 	procedure ChangeDir(s: in out Snake; dir: in Direction.Dir) is
-   begin
-   end ChangeDir;
+   	begin
+   	end ChangeDir;
 	
 	procedure AddPoint(s: in out Snake) is
-   begin
-   end AddPoint;
+   	begin
+   	end AddPoint;
    
 	function Score(s: in out Snake) return Integer is
-   begin
-   end Score;
+   	begin
+   	end Score;
    
-
 end Snake;
