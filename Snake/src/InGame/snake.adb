e15546c33ff593b9b2225904d3bbd99a81fa0424
raphael.treglia <32885615+TregliaRaphael@users.noreply.github.com>
2021-02-03 17:09:08 +0100

    Update snake.adb

--- Snake/src/InGame/snake.adb
+++ Snake/src/InGame/snake.adb
@@ -17,66 +17,85 @@ package body Snake is
 	
    	function Creat(ctxt: in out GameContext.Context) return Snake is
 	s : Snake;
-	e: SnakeElem;
    	begin
 		for i in 0 .. 4 loop
-			e.pos := Position'(5, 3);
-			e.color := None;
-			e.zoom := ctxt.Config.Zoom;
-			
-			if i = 0 and ctxt.Config.Color then
-				e.color := Blue;
-			elif ctxt.Config.Color then
-				e.color := Red;
-			end if;
-			s.elems.append(e);
+			s.elems.append(Position'(5, 3));
 		end loop;
-		return (ctxt, s, Down)
+		s.ctxt := ctxt;
+		s.dir := Down;
+		return s;
    	end Creat;
 	
    	procedure Display(s: in Snake) is
    	begin
-		case s.zoom is
+		for i in 0 .. s.elems.Length loop
+			Display(s.elems.Element(i), s.ctxt.Config.Zoom, s.ctxt.Config.Color);
+		end loop;
+   	end Display;
+	
+	procedure Display(elem: in SnakeElem; zm: GameContext.ZoomIndice; cl: ColorName) is
+   	begin
+		case zm is
 		when 1 =>
-			MoveTo(s.pos.X, s.pos.Y + 1);
-			if s.color /= None then
-				SetColor(s.color);
+			MoveTo(elem.X, elem.Y + 1);
+			if cl /= None then
+				SetColor(cl);
 			end if;
 		end case;
    	end Display;
-   
-	procedure Hide(s: in Snake) is
+	
+	procedure Hide(elem: in SnakeElem; zm: GameContext.ZoomIndice) is
    	begin
-		case s.zoom is
+		case zm is
 		when 1 =>
-			Print(" ", s.pos.X, s.pos.Y + 1);
+			Print_at(" ", s.pos.X, s.pos.Y + 1);
 		end case;
    	end Hide;
 	
 	procedure Move(s: in out Snake) is
+	head : SnakeElem;
    	begin
+		head := s.elems.First_Element;
+		head.Pos := 
+		
    	end Move;
    
 	procedure Pos(s: in out Snake; p: Position) is
    	begin
-		s.pos := p;
+		s.elems.First_Element.pos := p;
    	end Pos;
    
 	function Pos(s: in Snake) return Position is
    	begin
-		return s.pos;
+		return s.elems.First_Element.pos;
+   	end Pos;
+	
+	function Pos(elem: in SnakeElem) return Position is
+   	begin
+		return elem.pos;
    	end Pos;
    
 	procedure ChangeDir(s: in out Snake; dir: in Direction.Dir) is
+	p : Position;
    	begin
+		-- UP + DOWN == 0 (-1 + 1) && LEFT + RIGHT == 0 (-2 + 2)
+		if dir + s.dir /= 0 then
+			p := NextPosFrom(dir, Pos(s));
+			if s.elems.Element(1).pos.X = p.X and s.elems.Element(1).pos.Y = p.Y then
+				return;
+			end if;
+			s.dir = dir;
+		end if;
    	end ChangeDir;
 	
 	procedure AddPoint(s: in out Snake) is
    	begin
+		s.elems.append(s.elems.Last_Element);
    	end AddPoint;
    
 	function Score(s: in out Snake) return Integer is
    	begin
+		return s.elems.Length;
    	end Score;
    
 end Snake;
